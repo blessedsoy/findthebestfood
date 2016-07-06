38f03d7fa89f6f2e1486225cf3b609595bd5b9d2
GordonNY <gordonsdev@gmail.com>
2016-07-06 14:27:38 -0400

    add _parseRestaurantInfo method

--- lib/findthebestfood/google/google_api_helper.rb
+++ lib/findthebestfood/google/google_api_helper.rb
@@ -22,7 +22,22 @@ private
 		_parseRestaurantInfo(results)
 	end
 
-
+	def _parseRestaurantInfo(restaurants)
+		restaurants.collect do |restaurant|
+			# binding.pry
+			name = restaurant.name
+			rh = {}
+			rh[name] = {}
+			rh[name][:rating] = restaurant.rating || 0
+			rh[name][:price] = restaurant.price_level || 0
+			rh[name][:address] = restaurant.formatted_address || ""
+			rh[name][:phone] = restaurant.formatted_phone_number || ""
+			rh[name][:opening_hours] = {
+				'open_now': restaurant.opening_hours[:open_now],
+				'weekday': restaurant.opening_hours[:weekday_text] # => []
+			}
+			rh
+		end
 	end
 
 
