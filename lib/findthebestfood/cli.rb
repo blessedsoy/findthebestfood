7521dce087588eb789d92a6eccc6b7377eb79d43
blessedsoy <unmigong@gmail.com>
2016-07-07 13:25:05 -0400

    change in zagat.rb class structure

--- lib/findthebestfood/cli.rb
+++ lib/findthebestfood/cli.rb
@@ -2,10 +2,11 @@ class FindTheBestFood::CLI
 
   def call
      puts FindTheBestFood::YelpSort.find_by_yelp('padthai', 'midtown west, manhattan')
-     puts FindTheBestFood::Zagat.scrape('pad thai', 'midtown west')
+     puts FindTheBestFood::Zagat.results('pad thai', 'midtown west')
     results = Google::Scraper.scrapRestaurantInfoWith(location:"manhattan, new york", food:"chinese food")
     puts results
   end
+
 
 
 
--- lib/findthebestfood/zagat.rb
+++ lib/findthebestfood/zagat.rb
@@ -5,43 +5,50 @@ require 'open-uri'
 
 class FindTheBestFood::Zagat
 
+  attr_reader :food, :location
 
-  def self.scrape(food, location)
+  def initialize(food, location)
     @food = food
     @location = location
+  end
+
+  def full_url
     front_url = "https://www.zagat.com/"
-    type_of_food = "search/place?text=#{food.gsub(" ","+")}&rdr=1#filter/"
+    type_of_food = "search/place?text=#{@food.gsub(" ","+")}&rdr=1#filter/"
     
-    case location
+    case @location
       when 'midtown west'
-        location = "neighborhoods=West+30s%7CWest+40s%7CWest+50s"
+        @location = "neighborhoods=West+30s%7CWest+40s%7CWest+50s"
       when "upper west" 
-        location = "neighborhoods=West+60s%7CWest+70s%7CWest+80s%7CWest+90s"
+        @location = "neighborhoods=West+60s%7CWest+70s%7CWest+80s%7CWest+90s"
       when "midtown east"
-        location = "neighborhoods=East+40s%7CEast+50s"
+        @location = "neighborhoods=East+40s%7CEast+50s"
       when "upper east"
-        location = "neighborhoods=East+60s%7CEast+70s%7CEast+80s%7CEast+90s"
+        @location = "neighborhoods=East+60s%7CEast+70s%7CEast+80s%7CEast+90s"
       when "chelsea" || "flatiron"
-        location = "neighborhoods=Chelsea%7CFlatiron%7CMeatpacking+District%7CWest+30s"
+        @location = "neighborhoods=Chelsea%7CFlatiron%7CMeatpacking+District%7CWest+30s"
       when "murray hill" || "gramercy"
-        location = "neighborhoods=East+40s%7CGramercy+Park%7CNoMad%7CMurray+Hill"
+        @location = "neighborhoods=East+40s%7CGramercy+Park%7CNoMad%7CMurray+Hill"
       when "west village" || "greenwich village"
-        location = "neighborhoods=Greenwich+Village%7CNoHo%7CWest+Village"
+        @location = "neighborhoods=Greenwich+Village%7CNoHo%7CWest+Village"
       when "soho" || "tribeca" || "chinatown" || "little italy" 
-        location = "neighborhoods=Little+Italy%7CNolita%7CSoHo%7CTriBeCa"
+        @location = "neighborhoods=Little+Italy%7CNolita%7CSoHo%7CTriBeCa"
       when "financial district" || "battery park city"
-        location = "Battery+Park+City%7CFinancial+District&%7CSouth+Street+Seaport"
+        @location = "Battery+Park+City%7CFinancial+District&%7CSouth+Street+Seaport"
       when "lower east side" || "east village"
-        location = "neighborhoods=East+Village%7CLower+East+Side"
+        @location = "neighborhoods=East+Village%7CLower+East+Side"
       when "harlem" || "east harlem" || "morningside heights" 
-        location = "neighborhoods=East+90s%7CEast+Harlem%7CHarlem%7CMorningside+Heights"
+        @location = "neighborhoods=East+90s%7CEast+Harlem%7CHarlem%7CMorningside+Heights"
       else
-        location = "neighborhoods=West+30s%7CWest+40s%7CWest+50s%7CWest+60s%7CWest+70s%7CWest+80s%7CWest+90s%7CEast+40s%7CEast+50s%7CEast+60s%7CEast+70s%7CEast+80s%7CEast+90s%7CChelsea%7CFlatiron%7CMeatpacking+District%7CGramercy+Park%7CNoMad%7CMurray+Hill%7CGreenwich+Village%7CNoHo%7CWest+Village%7CLittle+Italy%7CNolita%7CSoHo%7CTriBeCa%7CBattery+Park+City%7CFinancial+District&%7CSouth+Street+Seaport%7CEast+Village%7CLower+East+Side%7CEast+Harlem%7CHarlem%7CMorningside+Heights"
+        @location = "neighborhoods=West+30s%7CWest+40s%7CWest+50s%7CWest+60s%7CWest+70s%7CWest+80s%7CWest+90s%7CEast+40s%7CEast+50s%7CEast+60s%7CEast+70s%7CEast+80s%7CEast+90s%7CChelsea%7CFlatiron%7CMeatpacking+District%7CGramercy+Park%7CNoMad%7CMurray+Hill%7CGreenwich+Village%7CNoHo%7CWest+Village%7CLittle+Italy%7CNolita%7CSoHo%7CTriBeCa%7CBattery+Park+City%7CFinancial+District&%7CSouth+Street+Seaport%7CEast+Village%7CLower+East+Side%7CEast+Harlem%7CHarlem%7CMorningside+Heights"
     end
 
       back_url = "&vertical=46&orderby=score_food&sort=desc&page=1"
-      full_url = front_url+type_of_food+location+back_url
+      full_url = front_url+type_of_food+@location+back_url
+      full_url
+    end
 
+  def scrape
       doc = Nokogiri::HTML(open(full_url))
       restaurants = Hash.new { |hash, key| hash[key] = {} }
 
@@ -58,10 +65,20 @@ class FindTheBestFood::Zagat
         end
       end
       restaurants = restaurants.sort_by {|restaurant, element| element[:rating]}
-      results = {}
-      restaurants.each do |restaurant|
-        results[restaurant[0]] = restaurant[1]
-      end
-      results
+      restaurants
   end
+
+  def final_hash
+    results = {}
+    scrape.each do |restaurant|
+      results[restaurant[0]] = restaurant[1]
+    end
+    results
+  end
+
+  def self.results(food, location)
+    zagat = self.new(food, location)
+    zagat.final_hash
+  end
+
 end
